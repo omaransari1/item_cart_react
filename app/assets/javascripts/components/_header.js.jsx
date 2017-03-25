b23b87615e0422a5f15dc80289a3c1e626c0f092
Omar Ansari <omaransari1@gmail.com>
2017-03-24 21:48:26 -0500

    changed img to img_url in css

--- app/assets/javascripts/components/_header.js.jsx
+++ app/assets/javascripts/components/_header.js.jsx
@@ -2,8 +2,9 @@ var Header = React.createClass({
     render() {
         return (
             <div className="headerstyling">
-                <h1 className="headerstyling">The Cat Shelter</h1>
+                <h1 className="headerstyling"><span className="catshelter">The Cat Shelter</span><span className="meow">MEOWW!!!</span></h1>
             </div>
         )
     }
 });
+
